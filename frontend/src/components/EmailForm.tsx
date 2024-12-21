0da5ae8ea6631f13d2e178d38ac9cc8a8b04b4b9
MeetBagda <bagdameet@gmail.com>
2024-12-21 15:41:03 +0530

    fix: ui fixed

--- frontend/src/components/EmailForm.tsx
+++ frontend/src/components/EmailForm.tsx
@@ -9,6 +9,16 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import OriginTextArea from "./OriginTextArea";
+import OriginButton from "./OriginButton";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { Check, Copy } from "lucide-react";
 
 interface FormValues {
   purpose: string;
@@ -21,11 +31,15 @@ interface FormValues {
 const EmailForm = () => {
   const methods = useForm<FormValues>({
     defaultValues: {
-      maxLength: 50,
+      maxLength: 100,
     },
   });
 
-  const { handleSubmit, watch, formState: { errors } } = methods;
+  const {
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = methods;
   const [generatedEmail, setGeneratedEmail] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -76,122 +90,181 @@ const EmailForm = () => {
       alert("Failed to copy email to clipboard.");
     }
   };
+  // const handleCopy = async () => {
+  //   if (!generatedEmail) return; // Prevent copying if no email generated
+
+  //   try {
+  //     await navigator.clipboard.writeText(generatedEmail);
+  //     alert("Email copied to clipboard!");
+  //   } catch (error) {
+  //     console.error("Error copying to clipboard:", error);
+  //     alert("Failed to copy email to clipboard.");
+  //   }
+  // };
+  //const [copied, setCopied] = useState<boolean>(false);
 
   return (
-    <div style={{ width: "auto" }}>
-      <div
-        className="email-container"
-        style={{ display: "flex", width: "100%" }}
-      >
-         <FormProvider {...methods}>
-          <form onSubmit={handleSubmit(onSubmit)} className="email-form">
-            <FormItem>
-              <FormLabel htmlFor="purpose">Purpose:</FormLabel>
-              <FormControl>
-                <Textarea
-                  id="purpose"
-                  {...methods.register("purpose")}
-                  required
-                  placeholder="Example: I am writing an email to my boss for salary increment."
-                />
-                
-              </FormControl>
-            </FormItem>
-           <FormItem>
-              <FormLabel htmlFor="subjectLine">Subject Line:</FormLabel>
-              <FormControl>
-                <Input
-                  type="text"
-                  id="subjectLine"
-                  {...methods.register("subjectLine")}
-                  required
-                  placeholder="Example: Product Review, etc..."
-                />
-                 
-              </FormControl>
-            </FormItem>
-            <FormItem>
-              <FormLabel htmlFor="recipients">
-                Recipients (comma-separated):
-              </FormLabel>
-              <FormControl>
-                <Input
-                  type="text"
-                  id="recipients"
-                  {...methods.register("recipients")}
-                  required
-                  placeholder="Example: hr.department@gmail.com"
-                />
-                
-              </FormControl>
-             </FormItem>
-            <FormItem>
-              <FormLabel htmlFor="senders">
-                Senders (comma-separated):
-              </FormLabel>
-              <FormControl>
-                <Input
-                  type="text"
-                  id="senders"
-                  {...methods.register("senders")}
-                  required
-                  placeholder="Example: manager.department@gmail.com"
-                />
-                
-              </FormControl>
-            </FormItem>
-            <FormItem>
-              <FormLabel htmlFor="maxLength">Max Length (words):</FormLabel>
-              <FormControl>
-                <Input
-                  type="range"
-                  id="maxLength"
-                  min="50"
-                  max="500"
-                  step="10"
-                  {...methods.register("maxLength", { valueAsNumber: true })}
-                />
-               
-              </FormControl>
-              <span>{watch("maxLength")}</span>
-            </FormItem>
-            <Button type="submit" disabled={loading}>
-              {loading ? "Generating..." : "Generate Email"}
-            </Button>
-            {error && <FormMessage>{error}</FormMessage>}
-              
-             
-          </form>
-        </FormProvider> 
-
-        {generatedEmail && (
-          <div className="email-output">
-            <h2>Generated Email:</h2>
-            <pre>{generatedEmail}</pre>
-            <Button onClick={copyToClipboard}>Copy</Button>
-          </div>
-        )}
+    <>
+      <div className="" style={{ width: "40%" }}>
+        <div
+          className="email-container w-full h-full flex flex-row justify-center items-center"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "start",
+          }}
+        >
+          <FormProvider {...methods}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="email-form w-full h-auto flex flex-col gap-8"
+            >
+              <FormItem className="w-full">
+                <FormLabel htmlFor="purpose">Purpose:</FormLabel>
+                <FormControl>
+                  {/* <OriginTextArea /> */}
+                  <Textarea
+                    id="purpose"
+                    {...methods.register("purpose")}
+                    required
+                    placeholder="Example: I am writing an email to my boss for salary increment."
+                    className="rounded-2xl"
+                  />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel htmlFor="subjectLine">Subject Line:</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    id="subjectLine"
+                    {...methods.register("subjectLine")}
+                    required
+                    placeholder="Example: Product Review, etc..."
+                    className="rounded-2xl"
+                  />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel htmlFor="recipients">
+                  Recipients (comma-separated):
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    id="recipients"
+                    {...methods.register("recipients")}
+                    required
+                    placeholder="Example: hr.department@gmail.com"
+                    className="rounded-2xl"
+                  />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel htmlFor="senders">
+                  Senders (comma-separated):
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    id="senders"
+                    {...methods.register("senders")}
+                    required
+                    placeholder="Example: manager.department@gmail.com"
+                    className="rounded-2xl"
+                  />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel htmlFor="maxLength">Max Length (words):</FormLabel>
+                <br />
+                <span>{watch("maxLength")}</span>
+                <FormControl>
+                  <Input
+                    type="range"
+                    id="maxLength"
+                    min="50"
+                    max="500"
+                    step="10"
+                    {...methods.register("maxLength", { valueAsNumber: true })}
+                  />
+                </FormControl>
+              </FormItem>
+              <Button type="submit" disabled={loading}>
+                {loading ? "Generating..." : "Generate Email"}
+              </Button>
+              {error && <FormMessage>{error}</FormMessage>}
+            </form>
+          </FormProvider>
+        </div>
       </div>
-    </div>
+      <div className="p-5" style={{ width: "60%" }}>
+        <div style={{ width: "100%" }}>
+          {generatedEmail && (
+            <div className="email-output">
+              
+              <pre style={{ whiteSpace: "pre-wrap", wordBreak: "break-word" }}>
+                {generatedEmail}
+              </pre>
+              <div className="mt-5">
+                <Button onClick={copyToClipboard}>Copy</Button>
+              </div>
+
+              {/* <div className="mt-5">
+              {
+               
+                <TooltipProvider delayDuration={0}>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="disabled:opacity-100"
+                        onClick={handleCopy}
+                        aria-label={copied ? "Copied" : "Copy to clipboard"}
+                        disabled={copied}
+                      >
+                        <div
+                          className={cn(
+                            "transition-all",
+                            copied
+                              ? "scale-100 opacity-100"
+                              : "scale-0 opacity-0"
+                          )}
+                        >
+                          <Check
+                            className="stroke-emerald-500"
+                            size={16}
+                            strokeWidth={2}
+                            aria-hidden="true"
+                          />
+                        </div>
+                        <div
+                          className={cn(
+                            "absolute transition-all",
+                            copied
+                              ? "scale-0 opacity-0"
+                              : "scale-100 opacity-100"
+                          )}
+                        >
+                          <Copy size={16} strokeWidth={2} aria-hidden="true" />
+                        </div>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent className="px-2 py-1 text-xs">
+                      Click to copy
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              }
+              </div> */}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
 export default EmailForm;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,6 @@ import Layout from "./components/layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
       <App />
-   
   </StrictMode>
 );
