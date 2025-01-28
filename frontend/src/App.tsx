0ffa999551f368e5a9cff9d8d54f77f1e71df9f5
MeetBagda <bagdameet@gmail.com>
2025-01-28 13:33:26 +0530

    feat: improve ui for email form and output

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,10 +11,23 @@ import {
 } from "react-router-dom";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/Signup";
-import { Children, useEffect } from "react";
+import {  useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [apiUrl, setApiUrl] = useState("");
+
+  useEffect(() => {
+      if (typeof window !== 'undefined') {
+            const hostname = window.location.hostname;
+             if (hostname === "localhost") {
+               setApiUrl(import.meta.env.VITE_API_URL_LOCAL)
+             } else {
+               setApiUrl(import.meta.env.VITE_API_URL_PRODUCTION)
+            }
+         }
+      }, []);
+
   return (
     <Router>
       <Routes>
@@ -22,7 +35,7 @@ function App() {
         <Route
           path="/email"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute apiUrl={apiUrl}>
             <Layout>
               <div className="flex flex-col w-full h-auto gap-2">
                 <Header />
@@ -34,14 +47,14 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/signin" element={<AuthRoute> <SignIn /> </AuthRoute>} />
-        <Route path="/signup" element={<AuthRoute><SignUp /></AuthRoute>} />
+        <Route path="/signin" element={<AuthRoute apiUrl={apiUrl}> <SignIn /> </AuthRoute>} />
+        <Route path="/signup" element={<AuthRoute apiUrl={apiUrl}><SignUp /></AuthRoute>} />
       </Routes>
     </Router>
   );
 }
 
-const AuthRoute = ({ children }: any) => {
+const AuthRoute = ({ children, apiUrl }: any) => {
   const navigate = useNavigate();
   useEffect(() => {
     const checkAuth = async () => {
@@ -50,7 +63,7 @@ const AuthRoute = ({ children }: any) => {
         if (!token) {
           return;
         }
-        await axios.get("https://ai-powered-email-generator.onrender.com/api/v1/user/me", {
+        await axios.get(`${apiUrl}/user/me`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -62,11 +75,11 @@ const AuthRoute = ({ children }: any) => {
     };
 
     checkAuth();
-  }, [navigate]);
+  }, [navigate, apiUrl]);
   return children;
 };
 
-const ProtectedRoute = ({ children }: any) => {
+const ProtectedRoute = ({ children, apiUrl }: any) => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
@@ -76,7 +89,7 @@ const ProtectedRoute = ({ children }: any) => {
         if (!token) {
           navigate("/signin");
         }
-        await axios.get("https://ai-powered-email-generator.onrender.com/api/v1/user/me", {
+         await axios.get(`${apiUrl}/user/me`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -86,7 +99,7 @@ const ProtectedRoute = ({ children }: any) => {
       }
     };
     checkAuth();
-  }, [navigate, token]);
+  }, [navigate, token, apiUrl]);
   return children;
 };
 
